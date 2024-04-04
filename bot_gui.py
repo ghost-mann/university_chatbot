91e58aec414340355fb85c7ea3f0b037b7523a0c
ghost-mann <iamaob8@protonmail.com>
2024-04-04 20:36:00 +0300

    feat: added horizontal scrollbar, reorganized contents of bot_gui.py window

--- bot_gui.py
+++ bot_gui.py
@@ -29,17 +29,20 @@ def send():
 # Create the main GUI window
 base = Tk()
 base.title("ChatBot - SL")
-base.geometry("400x500")
+base.geometry("990x660+50+50")
 base.resizable(width=FALSE, height=FALSE)
 
 # Create Chat window
 ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial", )
 ChatLog.config(state=DISABLED)
 
-# Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+# vertical scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="arrow", width=20)
 ChatLog['yscrollcommand'] = scrollbar.set
 
+# horizontal scrollbar
+h_scrollbar = Scrollbar(base, orient='horizontal', command=ChatLog.xview, width=20, cursor="arrow")
+ChatLog['xscrollcommand'] = h_scrollbar.set
 # Create Button to send message
 SendButton = Button(base, font=("Verdana", 12, 'bold'), text="Send", width="12", height=5,
                     bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff',
@@ -49,10 +52,11 @@ SendButton = Button(base, font=("Verdana", 12, 'bold'), text="Send", width="12",
 EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
 
 # Place all components on the screen
-scrollbar.place(x=376, y=6, height=386)
-ChatLog.place(x=6, y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+scrollbar.place(x=545, y=6, height=386)
+h_scrollbar.place(x=2, y=395, width=550)
+ChatLog.place(x=6, y=6, height=386, width=550)
+EntryBox.place(x=128, y=450, height=90, width=440)
+SendButton.place(x=6, y=450, height=90)
 
 # Start the GUI main loop
 base.mainloop()
